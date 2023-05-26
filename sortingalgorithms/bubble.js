3c72ab78a3ed96f9be75f5034cb1558f42d767b3
tarynries <taryn.ries@gmail.com>
2023-05-26 18:56:31 +0200

    sorting algorithms

---
+++ sortingalgorithms/bubble.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    const length = arr.length;
+
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+module.exports = bubbleSort;
